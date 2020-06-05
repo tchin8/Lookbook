611f7e1adbecb6563982c6f77f82303f0a03ca49
Tiffany <chin.tiffany108@gmail.com>
2020-06-05 11:18:48 -0400

    add post dropdown

--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -30,48 +30,82 @@ class PostIndex extends React.Component {
 
 
         {/* MOVE BACK TO INDEXITEM LATER */}
-        {/* <div className="post-dropdown dark">
+        <div className="post-dropdown dark">
           <div className="pdd-1-3 dark">
+
             <div className="save-post">
               <FontAwesomeIcon icon={['far', 'bookmark']}
                 className="fa-bookmark dark" />
 
-              <span>Save post
-                <span>Add this to your save items</span>
-              </span>
+              <div className="pdd-text">
+                <span>Save post</span>
+                <span className="pdd-t2">Add this to your save items</span>
+              </div>
             </div>
+
+            <div className="pdd-divider"></div>
 
             <div className="edit-post">
               <FontAwesomeIcon icon={['far', 'edit']}
                 className="fa-edit dark" />
+
+              <div className="pdd-text">
+                <span>Edit post</span>
+              </div>
             </div>
+
 
             <div className="edit-audience">
               <FontAwesomeIcon icon={['far', 'user-circle']}
                 className="fa-user-circle dark" />
+
+              <div className="pdd-text">
+                <span>Edit audience</span>
+              </div>
             </div>
+
 
             <div className="delete-post">
               <FontAwesomeIcon icon={['far', 'trash-alt']}
                 className="fa-trash-alt dark" />
+
+              <div className="pdd-text">
+                <span>Delete post</span>
+              </div>
             </div>
 
             <div className="turn-off-nots">
               <FontAwesomeIcon icon={['far', 'bell-slash']}
                 className="fa-bell-slash dark" />
+
+              <div className="pdd-text">
+                <span>Turn off notifications for this post</span>
+              </div>
             </div>
 
             <div className="change-date">
               <FontAwesomeIcon icon={['far', 'calendar-alt']}
                 className="fa-calendar-alt dark" />
+
+              <div className="pdd-text">
+                <span>Change date</span>
+              </div>
             </div>
+
+            <div className="pdd-divider"></div>
 
             <div className="hide-timeline">
               <FontAwesomeIcon icon={['far', 'times-circle']}
                 className="fa-times-circle dark" />
+
+              <div className="pdd-text2">
+                <span>Hide from timeline</span>
+                <span className="pdd-t2">This post may still appear in other places.</span>
+              </div>
             </div>
+
           </div>
-        </div> */}
+        </div>
 
         {posts.map(post => (
           <PostIndexItem 
