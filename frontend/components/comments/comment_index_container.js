bfd443c52282657423b875b1b16aabae7ead6b8c
Tiffany <chin.tiffany108@gmail.com>
2020-06-08 09:53:18 -0400

    add comment_index component

---
+++ frontend/components/comments/comment_index_container.js
@@ -0,0 +1,17 @@
+// import { connect } from 'react-redux';
+// import CommentIndexItem from './comment_index_item';
+// import PostIndex from './post_index';
+
+// const mSTP = state => ({
+//   post: Object.values(state.posts).reverse(),
+// });
+
+// const mDTP = dispatch => ({
+//   fetchPost: postId => dispatch(fetchPost(postId)),
+//   fetchUserPosts: userId => dispatch(fetchUserPosts(userId)),
+//   deletePost: postId => dispatch(deletePost(postId)),
+//   updatePost: post => dispatch(updatePost(post)),
+//   openModal: modal => dispatch(openModal(modal)),
+// });
+
+// export default connect(mSTP, mDTP)(PostIndex);
